5900da2150e49562850acc27ad37dad962136d94
Janco Tanis <git@jancology.com>
2024-01-29 17:17:21 +0100

    ruby formatting applied

--- lib/cloudally.rb
+++ lib/cloudally.rb
@@ -1,26 +1,27 @@
-require File.expand_path('../cloudally/configuration', __FILE__)
-require File.expand_path('../cloudally/api', __FILE__)
-require File.expand_path('../cloudally/client', __FILE__)
-require File.expand_path('../cloudally/version', __FILE__)
+require File.expand_path('cloudally/configuration', __dir__)
+require File.expand_path('cloudally/api', __dir__)
+require File.expand_path('cloudally/client', __dir__)
+require File.expand_path('cloudally/version', __dir__)
 
 module CloudAlly
-	extend Configuration
+  extend Configuration
 
-	# Alias for CloudAlly::Client.new
-	#
-	# @return [CloudAlly::Client]
-	def self.client(options={})
-		CloudAlly::Client.new(options)
-	end
+  # Alias for CloudAlly::Client.new
+  #
+  # @return [CloudAlly::Client]
+  def self.client(options = {})
+    CloudAlly::Client.new(options)
+  end
 
-	# Delegate to CloudAlly::Client
-	def self.method_missing(method, *args, &block)
-		return super unless client.respond_to?(method)
-		client.send(method, *args, &block)
-	end
+  # Delegate to CloudAlly::Client
+  def self.method_missing(method, *args, &block)
+    return super unless client.respond_to?(method)
 
-	# Delegate to CloudAlly::Client
-	def self.respond_to?(method, include_all=false)
-		return client.respond_to?(method, include_all) || super
-	end
+    client.send(method, *args, &block)
+  end
+
+  # Delegate to CloudAlly::Client
+  def self.respond_to?(method, include_all = false)
+    client.respond_to?(method, include_all) || super
+  end
 end
--- lib/cloudally/authentication.rb
+++ lib/cloudally/authentication.rb
@@ -1,65 +1,53 @@
 module CloudAlly
-	# Defines HTTP request methods
-	module Authentication
+  # Defines HTTP request methods
+  module Authentication
+    # Authorize to the CloudAlly portal and return access_token
+    def auth(options = {})
+      params = access_token_params.merge(options)
+      response = post("/auth", params)
+      # return access_token
+      process_token(response.body)
+    end
+    alias login auth
 
-		# Return an access token from authorization
-		def get_access_token(options={})
-			params = access_token_params.merge(options)
+    # Return an access token from authorization
+    def auth_refresh(token)
+      params = { refreshToken: token }
 
-			response = post("/auth/partner", params)
-			res = JSON.parse( response.body )
+      response = post("/auth/refresh", params)
+      # return access_token
+      process_token(response.body)
+    end
 
-			access_token = res[ "accessToken" ]
+    # Authorize to the partner portal and return access_token
+    def auth_partner(options = {})
+      params = access_token_params.merge(options)
+      response = post("/auth/partner", params)
+      # return access_token
+      process_token(response.body)
+    end
+    alias partner_login auth_partner
 
-			raise Exception.new 'Could not find valid accessToken' if access_token == '' || access_token.nil?
-			access_token
-		end
+    private
 
-		# Authorize to the CloudAlly portal and return access_token
-		def auth( options={} )
-			params = access_token_params.merge(options)
-			response = post("/auth", params)
-			# return access_token
-			process_token( response.body )
-		end
-		alias login auth
+    def access_token_params
+      {
+        email: username,
+        password: password
+      }
+    end
 
-		# Return an access token from authorization
-		def auth_refresh( token )
-			params = { refreshToken: token}
+    def process_token(response)
+      at = nil
+      CloudAlly.configure do |config|
+        at = config.access_token	= response["accessToken"]
+        config.token_type	        = response["tokenType"]
+        config.refresh_token		= response["refreshToken"]
+        config.token_expires		= response["expiresIn"]
+      end
+      raise StandardError.new 'Could not find valid accessToken; response ' + response.to_s if at == '' || at.nil?
 
-			response = post( "/auth/refresh", params )
-			# return access_token
-			process_token( response.body )
-		end
-
-		# Authorize to the partner portal and return access_token
-		def auth_partner( options={} )
-			params = access_token_params.merge(options)
-			response = post("/auth/partner", params)
-			# return access_token
-			process_token( response.body )
-		end
-		alias partner_login auth_partner
-
-		private
-
-		def access_token_params
-			{
-			  email: username,
-			  password: password
-			}
-		end
-		def process_token( response )
-			at = nil
-			CloudAlly.configure do |config|
-				at = config.access_token	= response[ "accessToken" ]
-				config.token_type			= response[ "tokenType" ]
-				config.refresh_token		= response[ "refreshToken" ]
-				config.token_expires		= response[ "expiresIn" ]
-			end
-			raise Exception.new 'Could not find valid accessToken; response '+response.to_s if at == '' || at.nil?
-			at
-		end
-	end
+      at
+    end
+  end
 end
